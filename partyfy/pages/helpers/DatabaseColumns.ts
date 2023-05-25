bf7a85622aa714c9c8b5828d7dc9e1c458a6ce21
Matthew Vandenberg <mv5903@gmail.com>
2023-05-25 15:00:23 -0400

    DIS DA SOUND OF A ROBOT

--- partyfy/pages/helpers/DatabaseColumns.ts
+++
@@ -1,5 +0,0 @@
-export async function getRecentsSchema() {
-    let response = await fetch('/api/database/recents?schema=true');
-    let json = await response.json();
-    return json.recordsets[0].map((item : any) => item.COLUMN_NAME);
-}
