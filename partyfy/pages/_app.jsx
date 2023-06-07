409b9f89d6b7a85aac5a5b2fd7b330cded87808e
Matthew Vandenberg <mv5903@gmail.com>
2023-06-07 11:01:04 -0400

    Add vercel metrics

--- partyfy/pages/_app.jsx
+++ partyfy/pages/_app.jsx
@@ -1,11 +1,13 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/globals.scss'
 import { UserProvider } from '@auth0/nextjs-auth0/client';
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }) {
   return (
   <UserProvider>
     <Component {...pageProps} />
+    <Analytics />
   </UserProvider>
   );
 }
