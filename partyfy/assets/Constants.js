2fbc2ce5076c4e5e86fd83291c7d6e375a660484
Matthew Vandenberg <mv5903@gmail.com>
2023-06-26 21:29:13 -0400

    Add missing playlist scopes

--- partyfy/assets/Constants.js
+++ partyfy/assets/Constants.js
@@ -2,7 +2,7 @@ const LOCAL_CONSTANTS = {
     SPOTIFY_CLIENT_ID : '56b011ba0994424ea55cd9f2205c6439',
     SPOTIFY_REDIRECT_URL : 'https://partyfy.mattvandenberg.com',
     SPOTIFY_API_URI : 'http://localhost:8080/',
-    SPOTIFY_SCOPES : 'user-modify-playback-state user-read-playback-state user-read-private user-read-email',
+    SPOTIFY_SCOPES : 'user-modify-playback-state user-read-playback-state playlist-read-private playlist-read-collaborative',
 }
 
 export const CONSTANTS = {
