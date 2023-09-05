4084ad6e3b3f94714d677fc9643b3064f7b22c57
Matthew Vandenberg <mv5903@gmail.com>
2023-09-05 13:13:35 -0400

    Lack of local file support causing crashes

--- partyfy/components/request/tabs/TheirSession.tsx
+++ partyfy/components/request/tabs/TheirSession.tsx
@@ -41,6 +41,7 @@ const TheirSession = ({ you, friendSpotifyAuth, friend } : { you: UserProfile, f
             let accessToken = await friendSpotifyAuth.getAccessToken();
             if (!accessToken) return;
             const response = await fetch('/api/spotify/nowplaying?access_token=' + accessToken);
+            if (response.status == 204) setNowPlaying(false);
             const data = await response.json();
             if (data) {
                 setIsLoading(false);
@@ -50,6 +51,7 @@ const TheirSession = ({ you, friendSpotifyAuth, friend } : { you: UserProfile, f
     }
 
     useEffect(() => {
+        showNowPlaying();
         const interval = setInterval(showNowPlaying, 1000);
         return () => clearInterval(interval);
     }, []);
@@ -60,120 +62,134 @@ const TheirSession = ({ you, friendSpotifyAuth, friend } : { you: UserProfile, f
         return () => clearInterval(interval);
     }, []);
 
+    if (nowPlaying === false) {
+        return <h3 className="text-center mt-4 text-2xl">No active session.</h3>
+    }
 
     return (
         <div>
-            {
-                queue.length > 0 
-                ?
-                <div className="w-full">
-                    <h5 className="mt-2 text-lg">Their Session</h5>
-                    <div className="flex flex-col items-center">
-                        <h3 className="text-3xl">Now Playing</h3>
-                        { 
-                            nowPlaying 
-                            ?                            
-                            <>
-                                <div className="card p-2 my-2 flex justify-center w-ful">
-                                    <div className="flex gap-2">
-                                        <div className="flex flex-col">
+            <div className="w-full">
+                <h5 className="mt-2 text-lg">Their Session</h5>
+                <div className="flex flex-col items-center">
+                    <h3 className="text-3xl">Now Playing</h3>
+                    { 
+                        nowPlaying 
+                        ?                            
+                        <>
+                            <div className="card p-2 my-2 flex justify-center w-ful">
+                                <div className="flex gap-2">
+                                    <div className="flex flex-col">
+                                        {
+                                            nowPlaying.currently_playing_type == 'track'
+                                            ?
+                                            nowPlaying.item.album.images.length > 0 &&
+                                            <img className="me-4" src={nowPlaying.item.album.images[0].url} style={{ width: '75px', height: '75px' }} />
+                                            :
+                                            nowPlaying.item
+                                            ?
+                                            <img className="me-4" src={nowPlaying.item.images[0].url} style={{ width: '75px', height: '75px' }} />
+                                            :
+                                            <img className="me-4" src={"https://www.freeiconspng.com/uploads/spotify-icon-2.png"} style={{ width: '75px', height: '75px' }} />
+                                        }
+                                        {
+                                            nowPlaying.item && nowPlaying.item.is_local &&
+                                            <img className="me-4" src={"https://www.freeiconspng.com/uploads/spotify-icon-2.png"} style={{ width: '75px', height: '75px' }} />
+                                        }
+                                        {
+                                            nowPlaying.item && !nowPlaying.item.is_local &&
+                                            <SpotifyLinkBack link={nowPlaying.item.external_urls.spotify} />
+                                        }
+                                    </div>
+                                    <div className="flex flex-col items-start justify-between w-full px-2">
+                                        <div className="flex justify-start">
                                             {
-                                                nowPlaying.currently_playing_type == 'track'
+                                                nowPlaying.item 
                                                 ?
-                                                <img className="me-4" src={nowPlaying.item.album.images[0].url} style={{ width: '75px', height: '75px' }} />
+                                                <h6 className="text-left"><strong>{nowPlaying.item.name + (nowPlaying.item.is_local ? ' (Local File)' : '')}</strong></h6>
                                                 :
+                                                <h6 className="text-left"><strong>Uknown {nowPlaying.currently_playing_type == 'episode' ? "(Podcasts not supported)" : ""}</strong></h6>
+                                            }
+                                            { nowPlaying.item && nowPlaying.item.explicit === true ? <BsExplicitFill className="mt-1 ms-2"/> : ''}
+                                        </div>
+                                            {
                                                 nowPlaying.item
                                                 ?
-                                                <img className="me-4" src={nowPlaying.item.images[0].url} style={{ width: '75px', height: '75px' }} />
+                                                <h6 className="text-left"><i>{nowPlaying.item.artists[0].name}</i></h6>
                                                 :
-                                                <img className="me-4" src={"https://www.freeiconspng.com/uploads/spotify-icon-2.png"} style={{ width: '75px', height: '75px' }} />
+                                                <h6 className="text-left"><i>Unknown artist</i></h6>
                                             }
+                                        <div className="flex justify-between w-full">
+                                            <h6>{fancyTimeFormat(nowPlaying.progress_ms)}</h6>
                                             {
-                                                nowPlaying.item &&
-                                                <SpotifyLinkBack link={nowPlaying.item.external_urls.spotify} />
+                                                nowPlaying.item
+                                                ?
+                                                <h6>{fancyTimeFormat(nowPlaying.item.duration_ms)}</h6>
+                                                :
+                                                <h6>?</h6>
                                             }
                                         </div>
-                                        <div className="flex flex-col items-start justify-between w-full px-2">
-                                            <div className="flex justify-start">
-                                                {
-                                                    nowPlaying.item 
-                                                    ?
-                                                    <h6 className="text-left"><strong>{nowPlaying.item.name}</strong></h6>
-                                                    :
-                                                    <h6 className="text-left"><strong>Uknown {nowPlaying.currently_playing_type == 'episode' ? "(Podcasts not supported)" : ""}</strong></h6>
-                                                }
-                                                { nowPlaying.item && nowPlaying.item.explicit === true ? <BsExplicitFill className="mt-1 ms-2"/> : ''}
-                                            </div>
-                                                {
-                                                    nowPlaying.item
-                                                    ?
-                                                    <h6 className="text-left"><i>{nowPlaying.item.artists[0].name}</i></h6>
-                                                    :
-                                                    <h6 className="text-left"><i>Unknown artist</i></h6>
-                                                }
-                                            <div className="flex justify-between w-full">
-                                                <h6>{fancyTimeFormat(nowPlaying.progress_ms)}</h6>
-                                                {
-                                                    nowPlaying.item
-                                                    ?
-                                                    <h6>{fancyTimeFormat(nowPlaying.item.duration_ms)}</h6>
-                                                    :
-                                                    <h6>?</h6>
-                                                }
-                                            </div>
-                                            <progress className="progress progress-flat-primary w-full" value={nowPlaying.progress_ms} max={nowPlaying.item ? nowPlaying.item.duration_ms : ''}></progress>
-                                        </div>
+                                        <progress className="progress progress-flat-primary w-full" value={nowPlaying.progress_ms} max={nowPlaying.item ? nowPlaying.item.duration_ms : ''}></progress>
                                     </div>
                                 </div>
-                            </>
-                            :
-                            <LoadingDots className="m-16" />
-                        }
-                    </div>
-                    <h4 className="mt-2 text-2xl">Up Next</h4>
-                    <h6 className="text-gray-600 mt-2" onClick={() => showQueueDisclaimer()}>Why is the queue inaccurate?</h6>
-                    <table className="table table-dark mt-3 w-full">
-                        <tbody>
+                            </div>
+                        </>
+                        :
+                        <>
                             {
-                                queue.map((item: any, index: number) => {
-                                    return (
-                                        <tr key={index}>
-                                            <th scope="row">{index + 1}</th>
-                                            <td>
-                                                <div className="flex items-center justify-between">
-                                                    <img className="me-2" src={item.type == 'track' ? item.album.images[2].url : item.images[0].url} style={{ width: '50px', height: '50px' }} />    
-                                                    <div className="w-2/3">
-                                                        <div className="flex justify-start">
-                                                            <h6 className="whitespace-normal w-3/4"><strong>{item.name}</strong></h6>
-                                                            {item.explicit === true ? <BsExplicitFill className="mt-1 ms-2"/> : ''}
-                                                        </div>
-                                                        <h6 className="whitespace-normal"><i>{item.type == 'track' ? item.artists[0].name : item.show.name}</i></h6> 
-                                                    </div>
-                                                    <SpotifyLinkBack link={item.external_urls.spotify} />
-                                                </div>
-                                            </td>
-                                        </tr>
-                                    );
-                                })
-
+                                nowPlaying === false
+                                ?
+                                <h3 className="text-center mt-4">Nothing is playing.</h3>
+                                :
+                                <LoadingDots className="m-16" />
                             }
-                        </tbody>
-                    </table>
+                        </>
+                    }
+                </div>
+                <h4 className="mt-2 text-2xl">Up Next</h4>
+                <h6 className="text-gray-600 mt-2" onClick={() => showQueueDisclaimer()}>Why is the queue inaccurate?</h6>
+                <table className="table table-dark mt-3 w-full">
+                    <tbody>
+                        {
+                            queue.map((item: any, index: number) => {
+                                return (
+                                    <tr key={index}>
+                                        <th scope="row">{index + 1}</th>
+                                        <td>
+                                            <div className="flex items-center justify-between">
+                                                <img className="me-2" src={item.type == 'track' ? item.album.images[2].url : item.images[0].url} style={{ width: '50px', height: '50px' }} />    
+                                                <div className="w-2/3">
+                                                    <div className="flex justify-start">
+                                                        <h6 className="whitespace-normal w-3/4"><strong>{item.name}</strong></h6>
+                                                        {item.explicit === true ? <BsExplicitFill className="mt-1 ms-2"/> : ''}
+                                                    </div>
+                                                    <h6 className="whitespace-normal"><i>{item.type == 'track' ? item.artists[0].name : item.show.name}</i></h6> 
+                                                </div>
+                                                <SpotifyLinkBack link={item.external_urls.spotify} />
+                                            </div>
+                                        </td>
+                                    </tr>
+                                );
+                            })
+
+                        }
+                    </tbody>
+                </table>
+            </div>
+            {
+                isLoading
+                ?
+                <div className="mt-4">
+                    <Loading  />
                 </div>
                 :
                 <>
-                    {
-                        isLoading
-                        ?
-                        <div className="mt-4">
-                            <Loading  />
-                        </div>
-                        :
-                        <>
-                            <h3 className="text-center mt-4">No active Spotify session.</h3>
-                            <h3 className="text-center mt-4">{`If ${friend.Username} is listening to a local file, their session will not appear here.`}</h3>
-                        </>
-                    }
+                {
+                    queue.length == 0 &&
+                    <>
+                        <h3 className="text-center mt-4">No queue available.</h3>
+                        <h3 className="text-center mt-4">{`If ${friend.Username} is listening to a local file, their session may not completely appear here.`}</h3>
+                    </>
+                }
                 </>
             }
         </div>
