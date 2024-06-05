357446896dc4e609bfb5c6f0cce5ce574af9cb09
Matthew Vandenberg <mv5903@gmail.com>
2024-06-05 15:08:22 -0700

    Send Notification to Home Assistant Devices when client experiences web errors

--- partyfy/pages/api/reportError.ts
+++ partyfy/pages/api/reportError.ts
@@ -8,13 +8,18 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     const errDetails = req.body;
 
     if (!errDetails) {
-        res.status(400).json({ message: 'Invalid request body, UserID key must be present.' });
+        res.status(400).json({ message: 'Request body must be provided.' });
         return;
     }
 
     console.log('Error reported:', errDetails);
     
+    // Add error to database
     await database.addNewClientError(errDetails);
+
+    // Notify myself via Home Assistant Automation
+    await fetch(process.env.HOME_ASSISTANT_ALERT_WEBHOOK_URL);
+
     res.status(200).json({ message: 'Error reported successfully' });
   } else {
     res.status(405).json({ message: 'Method not allowed' });
